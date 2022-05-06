c33c7f946d4f52209e8eae375e96754a2a6a870c
ayeo144 <ayeo144@googlemail.com>
2022-05-06 15:08:03 +0100

    Dockerfile syntax

---
+++ etl/src/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Env:
+    """
+    Store Environment variables.
+    """
+
+    NEWS_API_KEY = os.getenv("NEWS_API_KEY", None)
+    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY", None)
+    AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY", None)
+    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", None)
+    AWS_DB_INSTANCE_ID = os.getenv("AWS_DB_INSTANCE_ID", None)
+    AWS_DB_USERNAME = os.getenv("AWS_DB_USERNAME", None)
+    AWS_DB_PASSWORD = os.getenv("AWS_DB_PASSWORD", None)
+    AWS_DB_HOST = os.getenv("AWS_DB_HOST", None)
+    AWS_DB_PORT = os.getenv("AWS_DB_PORT", None)
+    AWS_DB_NAME = os.getenv("AWS_DB_NAME", None)
