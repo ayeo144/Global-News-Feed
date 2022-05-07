3d8b624498815e22a2286175acc17f1ca70ffa85
ayeo144 <ayeo144@googlemail.com>
2022-05-07 14:03:55 +0100

    Environment variables test

--- etl/tests/test_config.py
+++ etl/tests/test_config.py
@@ -12,12 +12,12 @@ def test_Env_variables():
     invalid_var = ["", None]
 
     assert Env.NEWS_API_KEY not in invalid_var
-    assert AWS_ACCESS_KEY not in invalid_var
-    assert AWS_SECRET_KEY not in invalid_var
-    assert S3_BUCKET_NAME not in invalid_var
-    assert AWS_DB_INSTANCE_ID not in invalid_var
-    assert AWS_DB_USERNAME not in invalid_var
-    assert AWS_DB_PASSWORD not in invalid_var
-    assert AWS_DB_HOST not in invalid_var
-    assert AWS_DB_PORT not in invalid_var
-    assert AWS_DB_NAME not in invalid_var
+    assert Env.AWS_ACCESS_KEY not in invalid_var
+    assert Env.AWS_SECRET_KEY not in invalid_var
+    assert Env.S3_BUCKET_NAME not in invalid_var
+    assert Env.AWS_DB_INSTANCE_ID not in invalid_var
+    assert Env.AWS_DB_USERNAME not in invalid_var
+    assert Env.AWS_DB_PASSWORD not in invalid_var
+    assert Env.AWS_DB_HOST not in invalid_var
+    assert Env.AWS_DB_PORT not in invalid_var
+    assert Env.AWS_DB_NAME not in invalid_var
