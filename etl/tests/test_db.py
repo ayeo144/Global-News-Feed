acb24cb308c76096d5e3ddf5460f6b8fb4c6fea6
ayeo144 <ayeo144@googlemail.com>
2022-05-07 14:18:11 +0100

    db connection tests

---
+++ etl/tests/test_db.py
@@ -0,0 +1,17 @@
+import sys
+from pathlib import Path
+
+SRC_DIR = Path(Path(__file__).parent.parent)
+sys.path.insert(0, str(SRC_DIR))
+
+from src.db import engine
+
+
+def test_db_connection():
+
+	try:
+		connection = engine.connect()
+	except Exception as e:
+		raise e
+	finally:
+		connection.close()
