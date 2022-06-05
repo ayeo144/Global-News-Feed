8d555cba8c7a6d7201df0f4bdf8f70895c7219c8
ayeo144 <ayeo144@googlemail.com>
2022-06-05 10:56:43 +0100

    Working on JS code for HTML page

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,3 +1,4 @@
+// Set up console logging to a specific HTML element
 window.console = {
     log: function(str){
         var node = document.createElement("div");
@@ -6,11 +7,35 @@ window.console = {
     }
 }
 
+// Create the template map and add to HTML element "map"
+var coords = [51.505, -0.09]
+var zoomLevel = 13
+var map = L.map('map').setView(coords, zoomLevel);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// Make the GET request to the backend REST API
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        console.log(xhr.responseText);
+
+// Display the results of the request
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+
+        const res = JSON.parse(xhr.responseText);
+        const data = res["data"];
+
+        // console.log(JSON.stringify(data[0], null, 4));
+
+        console.log(data[0]["Title"]);
+
+        // for (const item in data){
+        //     console.log(item)
+        // }
     }
-}
+};
+
 xhr.open("GET", "http://localhost:8081/records/")
+xhr.setRequestHeader('Accept', 'application/json');
 xhr.send(null);
